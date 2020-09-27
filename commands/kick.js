2d10c979d1b89f03b313b74922a7330e823ea5b8
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-27 16:52:20 -0400

    fix

--- commands/kick.js
+++ commands/kick.js
@@ -14,3 +14,24 @@ module.exports = message => {
     .catch(error => message.reply(`Sorry, an error occured.`))
 }
 }
+/*module.exports = {
+	name: 'kick',
+	description: 'Kick Members',
+	execute(message, args) {
+    if (message.channel.type == "dm") return;
+    const member = message.mentions.members.first()
+    if (message.member.hasPermission('KICK_MEMBERS')) {
+    if (!member) {
+      return message.reply(`Who are you trying to kick? You must mention a user.`)
+    }
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+	},
+};
+*/
---
+++ commands/server.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'server',
+	description: 'Server info',
+	execute(message, args) {
+    message.guild.members.fetch().then(fetchedMembers => {
+        const totalOnline = fetchedMembers.filter(member => member.presence.status === 'online');
+        const totalIdle = fetchedMembers.filter(member => member.presence.status === 'idle');
+        const totalDnd = fetchedMembers.filter(member => member.presence.status === 'dnd');
+        const totalAllOnline = totalDnd.filter(member => !member.user.bot).size + totalIdle.filter(member => !member.user.bot).size + totalOnline.filter(member => !member.user.bot).size;
+        const RealMembers = message.guild.members.cache.filter(member => !member.user.bot).size;
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nOnline members: ${totalAllOnline}\nReal Members: ${RealMembers}\nBots: ${message.guild.members.cache.filter(member => member.user.bot).size}`);
+        console.log(`${message.author.username} used !server`);
+      }
+	},
+};
