939f38efcd79c02bbd4e1f2931593e2ce37ecb9c
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-12 14:42:29 -0400

    Ok

---
+++ commands/avatar.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'avatar',
+	description: 'Shows users avatar',
+  aliases: ['pfp', 'icon'],
+	usage: '[member]',
+	execute(message, args) {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
+		});
+
+		message.channel.send(avatarList);
+	},
+};
--- commands/code.js
+++ commands/code.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'code',
 	description: 'Code',
+	aliases: ['github'],
 	execute(message, args) {
     message.channel.send(`https://github.com/ShrimpyStuff/Gifted-Bot/`);
   },
