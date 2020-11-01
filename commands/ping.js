f885651f8b68259539174c07f3312abd3cdc4b80
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-11-01 10:36:23 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,14 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping',
 	execute(message, args) {
-    message.reply(`Pong! Your ping is ${Date.now() - message.createdTimestamp} ms`);
+    message.channel.send("Pinging...").then(m =>{
+              var ping = (m.createdTimestamp - message.createdTimestamp) * 2;
+  
+              var embed = new Discord.MessageEmbed()
+              .setAuthor(`Pong, Your ping is ${ping}ms`)
+              .setColor("#00aae8")
+              
+              m.edit(embed)
+          });
   },
 };
