323d3c36cf1cdeadb411d87836e4b8d15c5a6395
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-03-24 12:59:54 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'commands',
 	description: 'Commands',
 	execute(message, args) {
-    message.channel.send('Commands at https://docs.google.com/document/d/1zaGQDYOkRH3JGtAeK1QVbFnwh8NdDQlH85V6IqHTbug/edit?usp=sharing');
+    message.channel.send('Commands at https://shrimpystuff.github.io/Gifted-Bot/');
   },
 };
